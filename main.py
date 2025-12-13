63cca5606b6aa3e13a50dd49dddddee7a61c276b
daria <d.musarskaya@gmail.com>
2025-12-13 20:30:08 +0300

    Добавлено основное окно запуска

---
+++ main.py
@@ -0,0 +1,28 @@
+import arcade
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "JumpQuest"
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        self.clear()
+
+
+def setup_game(width=900, height=600, title="JumpQuest"):
+    game = MyGame(width, height, title)
+    return game
+
+
+def main():
+    setup_game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
